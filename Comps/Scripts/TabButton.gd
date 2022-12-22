6d25abe588827cacfd257d07240d08c9a09e2ff5
Esoe B.Studios <myebstudios@gmail.com>
2022-12-22 23:00:21 +0100

    🏎️ Started design implementation
    - Organized folder structure
    - Added some assets (Icon, Logo, Font)
    - Created some components
        * ListView
        * Pill
        * TabBar
        * TabButton
        * TodoIten

---
+++ Comps/Scripts/TabButton.gd
@@ -0,0 +1,24 @@
+extends VBoxContainer
+
+
+@export var active = true
+@export var label = "Label"
+@export var count = "0"
+@export var color = Color()
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	updateText()
+	updateMode()
+
+
+func updateText():
+	$HBoxContainer/Label.text = label
+	$HBoxContainer/Pill.text = count
+
+
+func updateMode():
+	$ColorRect.visible = active
+	$ColorRect.color = color
+	
